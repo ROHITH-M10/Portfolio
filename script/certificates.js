50bc9d36e7727aa93600abcb3d6719f677272edd
ROHITH M <93328109+ROHITH-M10@users.noreply.github.com>
2024-06-02 14:55:08 +0530

    Add files via upload

---
+++ script/certificates.js
@@ -0,0 +1,22 @@
+function windowSizeFn() {
+    if (window.innerWidth < 780 ) {
+        console.log("HERE1");
+        document.querySelector(".main-nav-small").classList.remove("hidden");
+        document.querySelector(".main-nav").classList.add("hidden");
+    }
+    else {
+        console.log("HERE2");
+        document.querySelector(".main-nav").classList.remove("hidden");
+        document.querySelector(".main-nav-small").classList.add("hidden");
+    }    
+}
+
+function dropDownMenu() {
+    console.log("HERE3");
+    document.querySelector(".main-nav-small-inside").classList.toggle("hidden");
+}
+
+window.onload = windowSizeFn;
+window.addEventListener("resize", windowSizeFn);
+
+
