81b8afcf2beb0d195f45736d63337f0fd3280941
ROHITH M <93328109+ROHITH-M10@users.noreply.github.com>
2025-01-07 10:34:35 +0530

    Add files via upload

--- script/about.js
+++ script/about.js
@@ -24,34 +24,34 @@ window.addEventListener("resize", windowSizeFn);
 
 
 // Function to handle intersection
-function handleIntersection(entries, observer) {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.querySelector('.x-h2').style.color = 'white';
-        entry.target.querySelector('.x-p p').style.color = 'white';
-        entry.target.querySelector('.x-h2').style.transition = 'color 1s ease-in-out';
-        entry.target.querySelector('.x-p p').style.transition = 'color 1s ease-in-out';
-      } else {
-        entry.target.querySelector('.x-h2').style.color = '';
-        entry.target.querySelector('.x-p p').style.color = '';
-      }
-    });
-  }
+// function handleIntersection(entries, observer) {
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         entry.target.querySelector('.x-h2').style.color = 'white';
+//         entry.target.querySelector('.x-p p').style.color = 'white';
+//         entry.target.querySelector('.x-h2').style.transition = 'color 1s ease-in-out';
+//         entry.target.querySelector('.x-p p').style.transition = 'color 1s ease-in-out';
+//       } else {
+//         entry.target.querySelector('.x-h2').style.color = '';
+//         entry.target.querySelector('.x-p p').style.color = '';
+//       }
+//     });
+//   }
   
-  // Options for the intersection observer
-  const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.5 // Change the threshold as needed
-  };
+//   // Options for the intersection observer
+//   const options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.5 // Change the threshold as needed
+//   };
   
-  // Create the intersection observer
-  const observer = new IntersectionObserver(handleIntersection, options);
+//   // Create the intersection observer
+//   const observer = new IntersectionObserver(handleIntersection, options);
   
-  // Observe each section
-  document.querySelectorAll('.journey, .skills, .interest, .enjoy').forEach(section => {
-    observer.observe(section);
-  });
+//   // Observe each section
+//   document.querySelectorAll('.journey, .skills, .interest, .enjoy').forEach(section => {
+//     observer.observe(section);
+//   });
   
 
 
